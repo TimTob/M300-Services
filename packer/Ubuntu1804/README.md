9496558d0f654fd5cf47947602de5047795230e9
TimTob <tim.tobler@edu.tbz.ch>
2023-02-28 11:07:25 +0100

    comment

---
+++ packer/Ubuntu1804/README.md
@@ -0,0 +1,1 @@
+## Packer Beispiel von [serainville](https://github.com/serainville/packer_templates/tree/master/Ubuntu-18.04)
--- packer/Ubuntu1804/scripts/init.sh
+++ packer/Ubuntu1804/scripts/init.sh
@@ -7,14 +7,5 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 
-# Update the package list and upgrade the installed packages
-sudo apt update
-sudo apt upgrade -y
-
-# Install Apache2
-sudo apt install apache2 -y
-
-# Configure Apache2
-sudo ufw allow in "Apache Full"
-sudo systemctl enable apache2
-sudo systemctl start apache2
+apt update
+apt upgrade -y
